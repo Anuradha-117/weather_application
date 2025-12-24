278c19efa9e56607d7dc044d0f1edcdd2ef35145
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2025-12-24 10:33:15 +0530

    Added body UI with icons,responsives for devices and changed video background

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,5 @@
 console.log("JS loaded");
+
+fetch('http://api.weatherapi.com/v1/current.json?key=7c789b66ae5f48acaf871239252312&q=ratmalana&aqi=no')
+.then(res => res.json())
+.then(data => console.log(data))
